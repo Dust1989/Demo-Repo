69cfd9e2208efb837145b7f7fb3225aec23e0b2d
Dust-PC\Dust <destiny2013@yeah.net>
2019-05-02 17:28:36 +0800

    add some code

--- demo_01.py
+++ demo_01.py
@@ -2,18 +2,18 @@ import sys
 import requests
 
 
-# print(sys.version)
-# print(sys.executable)
+print(sys.version)
+print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = f"Hello, {who_to_greet}"
-#     return greeting
+def greet(who_to_greet):
+    greeting = f"Hello, {who_to_greet}"
+    return greeting
 
 
-# print(greet("dust"))
-# r = requests.get("https://www.baidu.com")
-# print(r.status_code)
+print(greet("dust"))
+r = requests.get("https://www.baidu.com")
+print(r.status_code)
 
 name = input("please input your name: ")
 print(name)
